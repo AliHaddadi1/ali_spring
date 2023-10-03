96f90734385b0a68fea81be1ce6e21a1a9132e8f
AliHaddadi1 <ali.haddadi@esprit.tn>
2023-10-03 12:28:54 +0200

    Les Associations

---
+++ src/main/java/tn/esprit/spring_ali/Entities/TypeChambre.java
@@ -0,0 +1,5 @@
+package tn.esprit.spring_ali.Entities;
+
+public enum TypeChambre {
+    SIMPLE, DOUBLE, TRIPLE
+}
---
+++ src/main/java/tn/esprit/spring_ali/SpringAliApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.spring_ali;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAliApplication.class, args);
+	}
+
+}
